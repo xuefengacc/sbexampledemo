bbdd65ce9c4fa86a5f9d789ce56660dc464ea1c9
xuefengacc <xuefeng.acc@gmail.com>
2020-03-04 13:59:18 -0500

    Init

---
+++ sbdemo/src/test/java/sbdemo/SbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
