ad229e4b9df2f3620a0798e80ba31d1aa6b939bc
xuefengacc <xuefeng.acc@gmail.com>
2020-03-06 15:02:23 -0500

    Create comsumer project

---
+++ base-consumer/src/main/java/baseconsumer/BaseConsumerApplication.java
@@ -0,0 +1,13 @@
+package baseconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseConsumerApplication.class, args);
+	}
+
+}
